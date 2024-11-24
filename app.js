e8c8dc0163ab22f07be330e790dc3e563789065b
VismayaSathyan <vismayasathyan157@gmail.com>
2024-11-24 13:46:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+"use strict";
+
+console.log(this);
+
+function x() {
+  console.log(this);
+  function y() {
+    console.log(this);
+  }
+  y();
+}
+
+x();
+
+const a = () => {
+  console.log(this, "a");
+  const b = () => {
+    console.log(this, "b");
+  };
+  b();
+};
+
+a();
+
+const obj = {
+  a: 10,
+  screen: function () {
+    const y = () => {
+      console.log(this);
+    };
+    y();
+  },
+};
+
+obj.screen();
